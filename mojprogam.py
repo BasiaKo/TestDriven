ff38d6826ea83d137fcc1e6223d3b291289199a8
BasiaKo <wojgosia@users.noreply.github.com>
2019-12-07 12:52:25 +0100

    zmiany

--- mojprogam.py
+++ mojprogam.py
@@ -1,3 +1,5 @@
+import math
+
 def zwroc_100():
     return 100
 
@@ -6,6 +8,18 @@ def zwroc_parametr(wynik):
 
 def dodaj(l1, l2):
     return l1+l2
+
+def mnozenie(a1, a2):
+    return a1*a2
+
+def odejmij(z1,z2):
+    return z1-z2
+
+def pierwiastek(z1):
+    return math.sqrt(z1)
+
+def potega(z1, z2):
+    return math.pow(z1, z2)
 
 def hello_world(zmienna):
     return zmienna
--- tdd1.py
+++ tdd1.py
@@ -15,8 +15,20 @@ class TestTDD(unittest.TestCase):
     def test_dodaj_8_15(self):
         self.assertEqual(mojprogam.dodaj(8,15), 23)
 
+    def test_mnozenie_7_8(self):
+        self.assertEqual(mojprogam.mnozenie(7, 8), 56)
+
     def test_hello_world(self):
         self.assertEqual(mojprogam.hello_world("Hello World"), "Hello World")
+
+    def test_odejmij_8_2(self):
+        self.assertEqual(mojprogam.odejmij(8,2), 6)
+
+    def test_pierwiastek_81(self):
+        self.assertEqual(mojprogam.pierwiastek(81), 9)
+
+    def test_potega2_3(self):
+        self.assertEqual(mojprogam.potega(2, 3), 8)
 
 if __name__=='__main__':
     unittest.main()
